29163587eedbf5af3c7943aedeff8f7793fc03ee
lusky3 <lusky3@users.noreply.github.com>
2026-03-30 02:15:38 -0400

    chore: bump version to 1.0.7

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "app.lusk.underseerr"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 8
-        versionName = "1.0.6"
+        versionCode = 9
+        versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
